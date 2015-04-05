00b61dd7a594aa43f29b032802be268339907586
quentinmayo <quentinmayo@yahoo.com>
2015-04-05 17:17:18 -0500

    removed netbeans files

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var api= require('./routes/api');
 app.use('/api_custom',api);
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
